24f757aed6dd886727f389cfa538072bef1d315b
Akif Küçükbenli <akifkucukbenli@gmail.com>
2023-10-20 23:07:48 +0300

    add codes for handly errors

--- bmi.py
+++ bmi.py
@@ -1,66 +1,85 @@
 import tkinter
 
+#make a tkinter window
 window = tkinter.Tk()
-window.minsize(400, 400)
-window.title("body mass index")
-window.config(bg="aliceblue")
-
-my_label = tkinter.Label(text="BMI Calculator", font=("Courier", 25, "bold"))
-my_label.config(padx=5, pady=5, bg="aquamarine")
-my_label.pack()
-
-my_label_2 = tkinter.Label(text="Enter Your Weight (kg)", font=("arial", 12, "bold"), bg="light grey")
-my_label_2.pack()
-
-my_entry = tkinter.Entry()
-
-my_entry.pack()
-
-my_label_3 = tkinter.Label(text="Enter Your Height (cm)", font=("arial", 12, "bold"), bg="light grey")
-my_label_3.config(padx=5, pady=5)
-my_label_3.pack()
-
-my_entry_2 = tkinter.Entry()
-
-my_entry_2.pack()
+window.minsize(400,400)
+window.title("bmi_calculator")
+window.config(pady=30,padx=30)
 
 
-def input_calculate():
-    y = my_entry_2.get()
-    x = my_entry.get()
-    # print(x)
-    # print(y)
+#x = weight
+#y = height
+#a = bmi
 
-    a = float(x) * 10 ** 4 / float(y) ** 2
-    print(a)
-    if a < 18.5:
-        message_ =  tkinter.Message(text="Your BMI is {:.2f}.You are underweight.".format(a))
-        message_.config(width=350, bg="aliceblue")
-        message_.place(x=70, y=300)
-    elif 18.5<=a<=25:
-        message_1 = tkinter.Message(text="Your BMI is {:.2f}.You are normal weight.".format(a) )
-        message_1.config(width=350, bg="aliceblue")
-        message_1.place(x=70, y=300)
-    elif 25 <a<=30:
-        message_2 = tkinter.Message(text="Your BMI is {:.2f}.You are overweight.".format(a))
-        message_2.config(width=350,bg="aliceblue")
-        message_2.place(x=70, y=300)
-    elif 30<a<40:
-        message_3 = tkinter.Message(text="Your BMI is {:.2f}.You are obese.".format(a))
-        message_3.config(width=350, bg="aliceblue")
-        message_3.place(x=70, y=300)
+#ui
+
+# ui title
+title_label = tkinter.Label(text="BMI Calculator", font=("Courier", 25, "bold"))
+title_label.config(bg="aquamarine")
+title_label.pack()
+
+# calculate bmi function with get a height and weight
+def calculate_bmi():
+    x = weight_input_entry.get()
+    y = height_input_entry.get()
+#normally calculate and handling erors method
+    if x == "" or y == "":
+        result_label.config(text="Enter both weight and height!..", bg="aliceblue")
     else:
-        message_4 = tkinter.Message(text="Your BMI is {:.2f}.You are morbidly obese.".format(a))
-        message_4.config(width=350, bg="aliceblue")
-        message_4.place(x=70, y=300)
+        try:
+            a = float(x) / (float(y)/100)**2
+            result_string = write_result(a)
+            result_label.config(text=result_string, bg="aliceblue")
+
+        except:
+            result_label.config(text="Enter a valid number!..", bg="aliceblue")
 
 
 
 
 
 
+# make a label for entry-weight
+weight_input_label = tkinter.Label(text="Enter your weight (kg)", font=("arial", 12, "bold"),bg="light grey")
+weight_input_label.pack()
+#make a entry for weight
+weight_input_entry = tkinter.Entry(width=30)
+weight_input_entry.pack()
+#make a label for entry-height
+height_input_label = tkinter.Label(text="Enter your height (cm)",font=("arial", 12, "bold"),bg="light grey")
+height_input_label.pack()
+#make a entry for height
+height_input_entry = tkinter.Entry(width=30)
+height_input_entry.pack()
+#make calculate button for calculate bmi
+calculate_button = tkinter.Button(text="Calculate",command=calculate_bmi)
+calculate_button.pack()
+#make the result label
+result_label = tkinter.Label()
+result_label.pack()
+# the function for result label with bmi result
+def write_result(a):
+    result_string = f" Your BMI is: {round(a,2)} . You are "
+    if a <= 16:
+        result_string += "severely thinnes."
 
-my_button = tkinter.Button(text="Calculate", command=input_calculate)
-my_button.place(x=175, y=200)
+    elif 16 < a < 17:
+        result_string += "moderate thinnes."
+    elif 17 <= a < 18.5:
+        result_string += "mild thinnes."
+    elif 18.5 <= a <= 25:
+        result_string += "normal weight."
+
+    elif 25 < a <= 30:
+        result_string += "overweight."
+    elif 30 < a <= 35:
+        result_string += "class 1 obesity."
+    elif 35 < a <= 40:
+        result_string += "class 2 obesity."
+    else:
+        result_string += "morbidly obesity."
+    return result_string
+
+
 
 window.mainloop()
